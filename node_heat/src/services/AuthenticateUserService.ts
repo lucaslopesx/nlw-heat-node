1294cd88d68138ead80011a262e69e374d2d1aa2
Lucas Lopes <lucas.lopesx1@gmail.com>
2021-10-20 11:33:06 -0300

    commit back-end nodejs finalizado

---
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -0,0 +1,89 @@
+import axios from "axios";
+import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken";
+/*
+  Receber code(string)
+  Recuperar access_token no github
+  Recuperar info do user no github
+  Verificar se o usuario existe no BD
+  caso:
+    Sim: Gerar token
+    Não: Criar usuario no BD e gerar token
+  Retornar token com as informacoes do usuario
+*/
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  name: string;
+  id: number;
+  login: string;
+  avatar_url: string;
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+    const response = await axios.get<IUserResponse>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          authorization: `Bearer ${accessTokenResponse.access_token}`,
+        },
+      }
+    );
+
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { token, user };
+  }
+}
+
+export { AuthenticateUserService };
